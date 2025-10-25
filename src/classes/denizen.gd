160263665d8da7cba83ce82cc33105bbd27ac0e6
Feymood <feymood@pm.me>
2025-10-25 16:56:12 +0200

    Add denizen class

---
+++ src/classes/denizen.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name Denizen
+## A denizen.
+
+# I'm not sure what type to extend this from right now; I think it'll need to be serializable, so
+# I picked Node but Resource could probably work as well
+
+enum Mood {
+	NORMAL,
+	
+}
+
+var denizen_name
+## The name of the denizen. Randomly generated
+var memory : Array = []
+## Record of recent notable events the denizen was a witness to
+var status :
